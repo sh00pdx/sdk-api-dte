16f1d5a41fa1be022834d6b16cdf5ac713c35938
Marcos Pulgar Acevedo <mpulgar@peoplefirst.cl>
2022-01-19 19:29:46 -0300

    fix error en URLS

--- src/Url.php
+++ src/Url.php
@@ -9,7 +9,7 @@ class Url {
     
     public function __construct()
     {
-        $URLS = [
+        $this->URLS = [
             'URL_BASE' => $_ENV['URL_BASE'] ?? 'ec2-54-172-105-92.compute-1.amazonaws.com/api/'
         ];
     }
